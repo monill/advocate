d52278a39ec88962157d5aa08bfa9a5fcc0eb259
John <juaorok@hotmail.com>
2023-01-29 19:38:25 -0300

    Create MaritalStatusController.php

---
+++ app/Http/Controllers/Dashboard/Configuration/MaritalStatusController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Configuration;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Configuration\MaritalStatusRequest;
+use App\Models\MaritalStatus;
+use Illuminate\Support\Facades\DB;
+
+class MaritalStatusController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        $statuses = DB::table('marital_status')->select('id', 'name')->get();
+        return view('dashboard.configurations.marital_statuses.index', compact('statuses'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.configurations.marital_statuses.create');
+    }
+
+    public function store(MaritalStatusRequest $request)
+    {
+        MaritalStatus::create($request->except('_token'));
+        return redirect()->route('marital-status.index');
+    }
+
+    public function edit($status_id)
+    {
+        $status = MaritalStatus::findOrFail($status_id);
+        return view('dashboard.configurations.marital_statuses.edit', compact('status'));
+    }
+
+    public function update(MaritalStatusRequest $request, $status_id)
+    {
+        MaritalStatus::findOrFail($status_id)->update($request->except('_token'));
+        return redirect()->route('marital-status.index');
+    }
+
+    public function destroy($status_id)
+    {
+        MaritalStatus::findOrFail($status_id)->delete();
+        return redirect()->route('marital-status.index');
+    }
+}
