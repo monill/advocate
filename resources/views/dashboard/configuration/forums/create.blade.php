e3313b5e0ce4be4cf841bc7ae46b41c7f0c50e0a
John <juaorok@hotmail.com>
2023-01-29 19:43:49 -0300

    Create create.blade.php

---
+++ resources/views/dashboard/configuration/forums/create.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Adicionar Fórum')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Adicionar Fórum</h4>
+                <p class="card-text">Para inserir um novo fórum, basta preencher os dados abaixo.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <form class="form form-vertical" action="{{ route('forums.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" name="name" id="name" maxlength="250" placeholder="Nome">
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-1">Adicionar</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
