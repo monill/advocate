0a6bc0ad982d7b3949e6c972bb4347cee904d4a9
John <juaorok@hotmail.com>
2023-01-29 19:44:21 -0300

    Create edit.blade.php

---
+++ resources/views/dashboard/configuration/professionals/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Editar Profissão')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Editar Profissão</h4>
+                <p class="card-text">Para alterar profissão, basta preencher os dados abaixo.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <form class="form form-vertical" action="{{ route('professionals.update', $professional->id) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" name="name" id="name" maxlength="250" placeholder="Nome" value="{{ $professional->name }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-1">Editar</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
