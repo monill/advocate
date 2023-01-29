a96f01c19e0221c5d8d7e0088a3a3d1a80f730d6
John <juaorok@hotmail.com>
2023-01-29 19:45:32 -0300

    Create edit.blade.php

---
+++ resources/views/dashboard/types/justices/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Editar Justiças')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Editar Justiças</h4>
+                <p class="card-text">Para alterar tipo de justiças, basta preencher os dados abaixo.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <form class="form form-vertical" action="{{ route('justices.update', $type->id) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="name">Nome:</label>
+                        <input type="text" id="name" class="form-control" name="name" placeholder="Nome" maxlength="250" value="{{ $type->name }}" />
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-1">Editar</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
