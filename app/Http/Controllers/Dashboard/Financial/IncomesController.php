6106bf51a447382c3f268c79a36acf01d76d8666
John <juaorok@hotmail.com>
2023-01-29 19:38:43 -0300

    Create IncomesController.php

---
+++ app/Http/Controllers/Dashboard/Financial/IncomesController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Financial;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IncomesController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        return view('dashboard.financials.incomes.index');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
