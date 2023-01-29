8fbdf18d9b1c83186467b83757e61000ec1f7aa3
John <juaorok@hotmail.com>
2023-01-29 19:37:34 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,19 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Api Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register Api routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "api" middleware group. Enjoy building your Api!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+//States-Cities
+Route::get('states', [\App\Http\Controllers\Api\CitiesController::class, 'states'])->name('api.states');
+Route::get('cities/{state_id}', [\App\Http\Controllers\Api\CitiesController::class, 'cities'])->name('api.cities');
