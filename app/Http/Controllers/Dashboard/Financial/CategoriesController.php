ca95d9e21555aa61375edb926d8b745b572fe92a
John <juaorok@hotmail.com>
2023-01-29 19:38:39 -0300

    Create CategoriesController.php

---
+++ app/Http/Controllers/Dashboard/Financial/CategoriesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Financial;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Financial\CategoriesRequest;
+use App\Models\FinancialCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        $categories = DB::table('financial_categories')->select('id', 'name')->get();
+        return view('dashboard.financials.categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.financials.categories.create');
+    }
+
+    public function store(CategoriesRequest $request)
+    {
+        FinancialCategory::create($request->except('_token'));
+        return redirect()->route('categories.index');
+    }
+
+    public function edit($category_id)
+    {
+        $category = FinancialCategory::findOrFail($category_id);
+        return view('dashboard.financials.categories.edit', compact('category'));
+    }
+
+    public function update(CategoriesRequest $request, $category_id)
+    {
+        FinancialCategory::findOrFail($category_id)->update($request->except('_token'));
+        return redirect()->route('categories.index');
+    }
+
+    public function destroy($category_id)
+    {
+        FinancialCategory::findOrFail($category_id)->delete();
+        return redirect()->route('categories.index');
+    }
+}
