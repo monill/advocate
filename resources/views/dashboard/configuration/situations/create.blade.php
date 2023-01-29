488aba8ce8e106d2ff08014f7c6cd9c04ce322a8
John <juaorok@hotmail.com>
2023-01-29 19:44:23 -0300

    Create create.blade.php

---
+++ resources/views/dashboard/configuration/situations/create.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Adicionar Situação')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Adicionar Situação</h4>
+                <p class="card-text">Para inserir uma nova situação, basta preencher os dados abaixo.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <form class="form form-vertical" action="{{ route('situations.store') }}" method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" name="name" id="name" maxlength="250" placeholder="Nome">
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-1">Adicionar</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
