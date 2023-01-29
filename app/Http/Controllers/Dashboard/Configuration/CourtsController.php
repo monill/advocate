6f522be06e8030bcb54e30556bf70d39d257b9cc
John <juaorok@hotmail.com>
2023-01-29 19:38:04 -0300

    Create CourtsController.php

---
+++ app/Http/Controllers/Dashboard/Configuration/CourtsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Configuration;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Configuration\CourtsRequest;
+use App\Models\Court;
+use Illuminate\Support\Facades\DB;
+
+class CourtsController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        $courts = DB::table('courts')->select('id', 'name')->get();
+        return view('dashboard.configurations.courts.index', compact('courts'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.configurations.courts.create');
+    }
+
+    public function store(CourtsRequest $request)
+    {
+        Court::create($request->except('_token'));
+        return redirect()->route('courts.index');
+    }
+
+    public function edit($court_id)
+    {
+        $court = Court::findOrFail($court_id);
+        return view('dashboard.configurations.courts.edit', compact('court'));
+    }
+
+    public function update(CourtsRequest $request, $court_id)
+    {
+        Court::findOrFail($court_id)->update($request->except('_token'));
+        return redirect()->route('courts.index');
+    }
+
+    public function destroy($court_id)
+    {
+        Court::findOrFail($court_id)->delete();
+        return redirect()->route('courts.index');
+    }
+}
