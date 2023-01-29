81121d7a556d15b536014b050345aaddc6aaae19
John <juaorok@hotmail.com>
2023-01-29 19:43:46 -0300

    Create edit.blade.php

---
+++ resources/views/dashboard/configuration/courts/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Editar Vara')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Editar Vara</h4>
+                <p class="card-text">Para alterar vara, basta preencher os dados abaixo.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <form class="form form-vertical" action="{{ route('courts.update', $court->id) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" name="name" id="name" maxlength="250" placeholder="Nome" value="{{ $court->name }}">
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-1">Editar</button>
+                    <button type="reset" class="btn btn-outline-secondary">Cancelar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
