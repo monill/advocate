36e6bab0719db48bb4094053d61bd0f6182eb957
John <juaorok@hotmail.com>
2023-01-29 19:40:06 -0300

    Create FailedLoginListener.php

---
+++ app/Listeners/FailedLoginListener.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\FailedLogin;
+use App\Notifications\FailedLoginNotification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Jenssegers\Agent\Agent;
+
+class FailedLoginListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $user_id = $event->user ? $event->user->id : null;
+
+        if (isset($event->user) && is_a($event->user, 'Illuminate\Database\Eloquent\Model')) {
+            $event->user->notify(new FailedLoginNotification());
+        }
+
+        $agent = new Agent();
+        //User Agent
+        $browser = $agent->browser() ? $agent->browser() : NULL;
+        //Operational System
+        $system = $agent->platform() ? $agent->platform() : NULL;
+
+        return FailedLogin::create([
+            'user_id' => $user_id,
+            'email' => $event->credentials['email'],
+            'ip' => request()->ip(),
+            'user_agent' => $browser,
+            'bot' => $agent->robot() == false ? null : $agent->robot(),
+            'os_family' => strtolower($system),
+            'os' => $system,
+            'browser_family' => strtolower($browser),
+            'browser' => $browser . " " . $agent->version($browser),
+            'is_desktop' => $agent->isDesktop(), //Is Desktop
+            'is_mobile' => $agent->isMobile(), //Is Mobile
+            'is_tablet' => $agent->isTablet(), //Is Tablet
+            'is_bot' => $agent->isRobot(), //Is Bot
+        ]);
+    }
+}
