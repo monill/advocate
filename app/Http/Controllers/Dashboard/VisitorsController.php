47e2381019ab8bb3c1ecf122d82c99f9c29f70ad
John <juaorok@hotmail.com>
2023-01-29 19:39:23 -0300

    Create VisitorsController.php

---
+++ app/Http/Controllers/Dashboard/VisitorsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VisitorsController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+}
