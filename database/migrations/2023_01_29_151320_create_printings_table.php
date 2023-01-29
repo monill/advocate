8e941e84dbf7e4bc37d08938c87f1092b9f8280d
John <juaorok@hotmail.com>
2023-01-29 16:14:27 -0300

    migration
    migration

--- database/migrations/2023_01_29_151320_create_printings_table.php
+++ database/migrations/2023_01_29_151320_create_printings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('prints', function (Blueprint $table) {
+        Schema::create('printings', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->index();
             $table->string('name');
