2cf9e75d092d374e55d2f2f289214a60e46ce189
John <juaorok@hotmail.com>
2023-01-29 19:38:31 -0300

    Create SituationsController.php

---
+++ app/Http/Controllers/Dashboard/Configuration/SituationsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Configuration;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Configuration\SituationsRequest;
+use App\Models\Situation;
+use Illuminate\Support\Facades\DB;
+
+class SituationsController extends Controller
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        $situations = DB::table('situations')->select('id', 'name')->get();
+        return view('dashboard.configurations.situations.index', compact('situations'));
+    }
+
+    public function create()
+    {
+        return view('dashboard.configurations.situations.create');
+    }
+
+    public function store(SituationsRequest $request)
+    {
+        Situation::create($request->except('_token'));
+        return redirect()->route('situations.index');
+    }
+
+    public function edit($situation_id)
+    {
+        $situation = Situation::findOrFail($situation_id);
+        return view('dashboard.configurations.situations.edit', compact('situation'));
+    }
+
+    public function update(SituationsRequest $request, $situation_id)
+    {
+        Situation::findOrFail($situation_id)->update($request->except('_token'));
+        return redirect()->route('situations.index');
+    }
+
+    public function destroy($situation_id)
+    {
+        Situation::findOrFail($situation_id)->delete();
+        return redirect()->route('situations.index');
+    }
+}
