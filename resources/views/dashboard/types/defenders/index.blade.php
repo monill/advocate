1931633f655f7392ea3b48bde87a7a102a7b7b21
John <juaorok@hotmail.com>
2023-01-29 19:45:28 -0300

    Create index.blade.php

---
+++ resources/views/dashboard/types/defenders/index.blade.php
@@ -0,0 +1,44 @@
+@extends('dashboard.layout.main')
+
+@section('subtitle', 'Tipos de Defensores')
+
+@section('content')
+
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <a href="{{ route('defenders.create') }}" class="btn btn-primary pull-right">Adicionar</a>
+                <h4 class="card-title">Tipos de Defensores</h4>
+                <p class="card-text">Gerencie aqui os tipos de defensores presentes no website.</p>
+            </div>
+            <div class="card-body">
+                @includeIf('errors.errors', [$errors])
+                @include('errors.messages')
+                <table class="table table-bordered table-hover" id="datatable">
+                    <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Opções</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($types as $type)
+                        <tr>
+                            <td>{{ $type->name }}</td>
+                            <td>
+                                <a href="{{ route('defenders.edit', $type->id) }}" data-toggle="tooltip" data-original-title="Editar">
+                                    <i data-feather="edit" class="m-r-5 text-success"></i>
+                                </a>
+                                <a href="javascript:void(0);" data-toggle="tooltip" data-original-title="Deletar">
+                                    <i data-feather="trash-2" class="mr-4 text-danger"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
