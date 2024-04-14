3fb23babd00c0877f407b3090ad3daa39b2d3f8a
John <juaorok@hotmail.com>
2024-04-14 17:28:59 -0300

    Update main.blade.php

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html lang="en" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template-starter">
+<html lang="pt-br" class="light-style layout-navbar-fixed layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template-starter">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"/>
